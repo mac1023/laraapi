87872f1160d790a9ad5fd8f2c5f3ad65fb6b1a17
zhangyajie <z_yajie@sina.com>
2018-09-28 16:18:45 +0800

    add user register interface

--- app/Http/Controllers/Api/VerificationCodesController.php
+++ app/Http/Controllers/Api/VerificationCodesController.php
@@ -9,7 +9,6 @@ use Overtrue\EasySms\EasySms;
 class VerificationCodesController extends Controller
 {
     public function store(VerificationCodeRequest $request, EasySms $easySms)
-//    public function store()
     {
         $phone = $request->phone;
 
@@ -23,6 +22,7 @@ class VerificationCodesController extends Controller
             // 生成4位随机数，左侧补0
             $code = str_pad(random_int(1, 9999), 4, 0, STR_PAD_LEFT);
 
+            $code = '1234';
             try {
                 $result = $easySms->send($phone, [
                     'content'  =>  "【Lbbs社区】您的验证码是{$code}。如非本人操作，请忽略本短信"
@@ -32,9 +32,6 @@ class VerificationCodesController extends Controller
                 return $this->response->errorInternal($message ?? '短信发送异常');
             }
         }
-
-
-        //呵呵
 
         $key = 'verificationCode_'.str_random(15);
         $expiredAt = now()->addMinute(10);
